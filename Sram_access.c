d3ea05bc235c5dd7321310d5831992a798db8bbc
hongrueil <64222945+hongrueil@users.noreply.github.com>
2023-09-21 15:29:57 +0800

    Update Sram_access.c
    add access func.
    add emulate _asm in line156

--- Sram_access.c
+++ Sram_access.c
@@ -3,46 +3,34 @@
 #include <stdio.h>
 
 extern void shutdown_asm(void);
+
 void set_freq(uint8_t clk);
+__attribute__((ramfunc))void access(uint8_t *StartAddress);
 void initialize_port(void);
+
+
+
+
+
 
 void main(void) {
   WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
-  initialize_port();
-  set_freq(1);
-  int i = 0;
-  int j = 0;
-  uint8_t data = 0x1;
   uint8_t arr[2049];
-  uint8_t *arr_ptr = arr;
-  // initialize arr
-  for (i = 0; i < 2048; ++i) {
-    *arr_ptr++ = data;
-  }
-  data = 0x2;
-  // access 512 * 2048 = 1048576 byte
-  for (i = 0; i < 512; ++i) {
-    arr_ptr = arr;
-    for (j = 0; j < 2048; ++j) {
-      *arr_ptr = data; //FRAM write
-      //data = *arr_ptr; //FRAM read
-      arr_ptr++;
-    }
-    *arr_ptr = data; // avoid compiler optimization
-  }
+  initialize_port();
+  set_freq(24);
+  access(arr);
   shutdown_asm();
 }
-
 // set system clk to 1, 8, 16, 24
 void set_freq(uint8_t clk) {
   CSCTL0_H = CSKEY_H;                     // Unlock CS registers
   if(clk == 1) {
-    CSCTL0_H = CSKEY_H;  
+    CSCTL0_H = CSKEY_H;
     CSCTL1 = DCORSEL;                                     //  set DCO to 1 MHZ
     CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;  // Set SMCLK = MCLK = DCO
                                                           // ACLK = VLOCLK
     CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;                 // Set all dividers to 1
-    CSCTL0_H = 0; 
+    CSCTL0_H = 0;
     __delay_cycles(60);
 
   } else if (clk == 8) {
@@ -60,16 +48,16 @@ void set_freq(uint8_t clk) {
     // Delay by ~10us to let DCO settle. 60 cycles = 20 cycles buffer + (10us / (1/4MHz))
     __delay_cycles(60);
     CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;   // Set all dividers to 1 for 8MHz operation
-    CSCTL0_H = 0;                           // Lock CS Registers 
+    CSCTL0_H = 0;                           // Lock CS Registers
 
   } else if (clk == 16) {
-    CSCTL0_H = CSKEY_H;  
+    CSCTL0_H = CSKEY_H;
     // Configure one FRAM waitstate as required by the device datasheet for MCLK
     // operation beyond 8MHz _before_ configuring the clock system.
     FRCTL0 = FRCTLPW | NWAITS_1;
-    
+
     CSCTL1 = DCOFSEL_0;                     // Set DCO to 1MHz
-    
+
     CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;        // Set SMCLK = MCLK = DCO, ACLK = VLOCLK
 
     // Per Device Errata set divider to 4 before changing frequency to
@@ -83,10 +71,10 @@ void set_freq(uint8_t clk) {
     CSCTL0_H = 0;
 
   } else if (clk == 24) {
-    CSCTL0_H = CSKEY_H;  
+    CSCTL0_H = CSKEY_H;
 
     FRCTL0 = FRCTLPW | NWAITS_2;
-    CSCTL1 = DCOFSEL_0;   
+    CSCTL1 = DCOFSEL_0;
     CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
     CSCTL3 = DIVA__4 | DIVS__4 | DIVM__4;   // Set all corresponding clk sources to divide by 4 for errata
     CSCTL1 = DCOFSEL_6 | DCORSEL;      // Set DCO to 24MHz
@@ -119,28 +107,59 @@ void initialize_port(void) {
   P7OUT = 0x00;
 
   P8DIR = 0xFF;
-  P8OUT = 0x00;    
+  P8OUT = 0x00;
 
   P9DIR = 0xFF;
-  P9OUT = 0x00; 
+  P9OUT = 0x00;
 
   PADIR = 0xFF;
-  PAOUT = 0x00; 
+  PAOUT = 0x00;
 
   PBDIR = 0xFF;
-  PBOUT = 0x00; 
+  PBOUT = 0x00;
 
   PCDIR = 0xFF;
-  PCOUT = 0x00; 
+  PCOUT = 0x00;
 
   PDDIR = 0xFF;
-  PDOUT = 0x00; 
+  PDOUT = 0x00;
 
   PEDIR = 0xFF;
-  PEOUT = 0x00; 
+  PEOUT = 0x00;
 
   // Disable the GPIO power-on default high-impedance mode to activate
   // previously configured port settings
   PM5CTL0 &= ~LOCKLPM5;
 }
-  
+/**********************************************************************//**
+ * @brief  Performs 512 time * 2048 byte access
+ *
+ * @param  StartAddress: For access address
+ *
+ * @return none
+ *************************************************************************/
+
+void access(uint8_t *StartAddress) {
+  uint8_t *sram_ptr;
+  uint8_t data = 0x12; //1 byte = 8 bits
+  int i = 0;
+  int j = 0;
+  sram_ptr =  StartAddress;
+  // initialize arr
+  for (j = 0; j < 2048; ++j) {
+    *sram_ptr++ = data;
+  }
+  // access 512 * 2048 byte
+  for (i = 512; i > 0; --i) {
+    sram_ptr =  StartAddress;
+    for (j = 0; j < 2048; ++j) {
+      //__asm("\t   MOVA\t   0x0004(SP),R15"); // for emulate 
+      *sram_ptr = data; // write
+      //data = *sram_ptr; // read
+      sram_ptr++;
+    }
+  }
+
+}
+
+
